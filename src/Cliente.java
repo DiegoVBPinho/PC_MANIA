e3d0d3746d71c5409e9581e018312517ea8fd4ab
DiegoVBPinho <dvbpinho@gmail.com>
2026-04-01 21:16:53 -0300

    Criei a classe Cliente.java

---
+++ src/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+    //atributos
+    private String nome;
+    private String cpf;
+
+    //construtor
+
+    Public Cliente (String nome, String cpf)
+    {
+
+    }
+
+    //métodos
+
+
+
+
+
+            //getters
+
+            //setters
+
+
+
+}
