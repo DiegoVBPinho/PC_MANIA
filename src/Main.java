57ddcad9f0f00791557c16c446a8d7d469eceda0
DiegoVBPinho <dvbpinho@gmail.com>
2026-04-04 15:58:20 -0300

    Organização da Main com comentários.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) {
+
+
+        //Configurações Iniciais
         int matricula = 537;
-
-
         ProcessarPedido.promocoes = new Computador[3];
 
+        //Montagem das Promoções
+
+
+        // Promoção 1: Apple
         HardwareBasico[] hardware = new HardwareBasico[3];
         hardware[0] = new HardwareBasico("Pentium Core i3", 220);
         hardware[1] = new HardwareBasico("Memória RAM", 8);
@@ -16,33 +21,35 @@ public class Main {
         ProcessarPedido.promocoes[0] = new Computador("Apple", matricula, new SistemaOperacional("macOS Sequoia", 64),hardware);
         ProcessarPedido.promocoes[0].addMemoriaUSB(new MemoriaUSB("Pen-drive", 16));
 
+
+        // Promoção 2: Samsumg
         hardware = new HardwareBasico[3];
         hardware[0] = new HardwareBasico("Pentium Core i5", 220);
         hardware[1] = new HardwareBasico("Memória RAM", 16);
         hardware[2] = new HardwareBasico("HD", 1);
 
-
         ProcessarPedido.promocoes[1] = new Computador("SAMSUNG", matricula+1234, new SistemaOperacional("Windows 8", 64),hardware);
         ProcessarPedido.promocoes[1].addMemoriaUSB(new MemoriaUSB("Pen-drive", 32));
 
-
+        // Promoção 3: Dell
         hardware = new HardwareBasico[3];
         hardware[0] = new HardwareBasico("Pentium Core i7", 4500);
         hardware[1] = new HardwareBasico("Memória RAM", 32);
         hardware[2] = new HardwareBasico("HD", 2);
 
-
         ProcessarPedido.promocoes[2] = new Computador("Dell", matricula+5678, new SistemaOperacional("Windows 10", 64),hardware);
         ProcessarPedido.promocoes[2].addMemoriaUSB(new MemoriaUSB("Pen-drive", 32));
 
+
+        // Preparação de Atendimento
         Cliente client = new Cliente("Diego Victor", "0900");
-
         ProcessarPedido processa = new ProcessarPedido(); // instacia de Pedidos
-
         Scanner input = new Scanner(System.in);
+
+        // Menu de Interação
+
         while(true)
         {
-
             System.out.println("Digite a Promocao que voce deseja adquirir(1, 2 ou 3): ");
             System.out.println("Digite 0, caso deseje finalizar sua compra! ");
 
@@ -53,10 +60,12 @@ public class Main {
             }
 
             processa.adicionarPedido(opcao); //Acumulativo de computadores que foram comprados pelo cliente
-
         }
 
         //Momento de enviar o pedido e escrever na tela o total de compra, os dados do Cliente e as promoções adquiridas.
+
+
+        // Finalização e Impressão do Recibo
 
         System.out.println();
         System.out.println(client.getNome());
@@ -66,25 +75,6 @@ public class Main {
         client.setComputadores(computadors);
         System.out.println("Total de Compra R$: " + client.calculaTotalCompra());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        input.close(); // linha pra fechar o Scanner.
     }
-
 }
